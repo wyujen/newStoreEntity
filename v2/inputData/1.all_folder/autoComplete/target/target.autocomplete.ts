70181f6dcec390f985792204275ab982f52caa6f
wyujen <wyujen0212@gmail.com>
2025-09-03 15:04:00 +0800

    fix

---
+++ v2/inputData/1.all_folder/autoComplete/target/target.autocomplete.ts
@@ -0,0 +1,114 @@
+// import
+Signal, signal
+import { createAutocompleteDisplaySignal, createFlexibleSearchSignal, SearchParam } from 'apps/frontend/src/app/service/other/search.signal.service';
+
+
+
+// const 
+targetSearchParam = signal<SearchParam>({
+    state: 'search',
+    optionList: [
+      {
+        enabled: true,
+        key: 'deleted',
+        type: 'boolean',
+        value: false,
+        mold: 'AND'
+      }
+    ]
+  })
+  targetFilteredList: Signal<any[]>
+  displayWithTarget: Signal<(id: string) => string>;
+
+// constructor{}
+  this.targetFilteredList = createFlexibleSearchSignal(this.targetSS.totalTargetListSignal, this.targetSearchParam)
+    this.Form.valueChanges.subscribe((data) => {
+      this.targetSearchParam.set({
+        state: 'search',
+        optionList: [
+          {
+            enabled: true,
+            key: 'deleted',
+            type: 'boolean',
+            value: false,
+            mold: 'AND'
+          },
+          {
+            enabled: true,
+            key: 'name',
+            type: 'keyword',
+            value: data.targetId,
+            mold: 'OR'
+          },
+          {
+            enabled: true,
+            key: 'id',
+            type: 'keyword',
+            value: data.targetId,
+            mold: 'OR'
+          }
+        ]
+      })
+    })
+    this.displayWithTarget = createAutocompleteDisplaySignal(this.targetSS.totalTargetRecordSignal, 'name')
+
+/////////////////////// no form /////////////////////////////
+// no form const 
+
+selectedtargetId: string = '';
+
+
+targetSearchParam = signal<SearchParam>({
+  state: 'search',
+  optionList: [
+    {
+      enabled: true,
+      key: 'deleted',
+      type: 'boolean',
+      value: false,
+      mold: 'AND'
+    }
+  ]
+})
+targetFilteredList: Signal<any[]>
+displayWithTarget: Signal<(id: string) => string>;
+
+// constructor{}
+
+this.targetFilteredList = createFlexibleSearchSignal(this.targetSS.totalTargetListSignal, this.targetSearchParam)
+
+this.displayWithTarget = createAutocompleteDisplaySignal(this.targetSS.totalTargetRecordSignal, 'name')
+
+
+// function
+
+setTargetSearchParam(){
+  this.targetSearchParam.set({
+    state: 'search',
+    optionList: [
+      {
+        enabled: true,
+        key: 'deleted',
+        type: 'boolean',
+        value: false,
+        mold: 'AND'
+      },
+      {
+        enabled: true,
+        key: 'name',
+        type: 'keyword',
+        value: this.selectedTargetId,
+        mold: 'OR'
+      },
+      {
+        enabled: true,
+        key: 'id',
+        type: 'keyword',
+        value: this.selectedTargetId,
+        mold: 'OR'
+      }
+    ]
+  })
+
+}
+
